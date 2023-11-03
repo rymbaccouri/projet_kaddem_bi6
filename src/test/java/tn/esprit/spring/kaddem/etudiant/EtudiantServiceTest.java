3cafe6d385db63e0e875722b199cd38170b5f2df
rymbaccouri <rym.baccouri@esprit.tn>
2023-11-03 10:54:19 +0100

    mise a jour

--- src/test/java/tn/esprit/spring/kaddem/etudiant/EtudiantServiceTest.java
+++ src/test/java/tn/esprit/spring/kaddem/etudiant/EtudiantServiceTest.java
@@ -17,6 +17,8 @@ import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
 import tn.esprit.spring.kaddem.services.EtudiantServiceImpl;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,114 +34,78 @@ public class EtudiantServiceTest {
     @Mock
     private EtudiantRepository etudiantRepository;
 
-    @Mock
-    private ContratRepository contratRepository;
-    @Mock
-    private EquipeRepository equipeRepository;
-    @Mock
-    private DepartementRepository departementRepository;
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
-
     @Test
-
     public void testAddEtudiant() {
-
-        Etudiant etudiant = new Etudiant();
-        etudiant.setNomE("John");
-        etudiant.setPrenomE("Doe");
-        etudiant.setOp(Option.SIM);
-
-
-        Contrat contrat = new Contrat();
-        Equipe equipe = new Equipe();
-        Departement departement = new Departement();
+        Etudiant etudiant = new Etudiant( "rym", "baccouri", Option.GAMIX);
+        etudiant.setNomE("rym");
+        etudiant.setPrenomE("baccouri");
+        etudiant.setOp(Option.GAMIX);
 
 
-        Mockito.when(etudiantRepository.save(Mockito.any(Etudiant.class))).thenReturn(etudiant);
-        Mockito.when(contratRepository.save(Mockito.any(Contrat.class))).thenReturn(contrat);
-        Mockito.when(equipeRepository.save(Mockito.any(Equipe.class))).thenReturn(equipe);
-        Mockito.when(departementRepository.save(Mockito.any(Departement.class))).thenReturn(departement);
 
 
-        Etudiant savedEtudiant = etudiantService.addEtudiant(etudiant);
 
 
-        assertNotNull(savedEtudiant);
-        assertEquals("John", savedEtudiant.getNomE());
-        assertEquals("Doe", savedEtudiant.getPrenomE());
-        assertEquals(Option.SIM, savedEtudiant.getOp());
+        when(etudiantRepository.save(etudiant)).thenReturn(etudiant);
+
+        Etudiant result = etudiantService.addEtudiant(etudiant);
 
 
+        verify(etudiantRepository, times(1)).save(etudiant);
+
+
+        assertNotNull(result);
+
+
+        assertEquals(etudiant, result);
     }
 
-
-   @Test
-    public void testEditEtudiant() {
-
-        Etudiant etudiant = new Etudiant();
-        etudiant.setNomE("John");
-        etudiant.setPrenomE("Doe");
-        etudiant.setOp(Option.SIM);
-
-        Contrat contrat = new Contrat();
-        Equipe equipe = new Equipe();
-        Departement departement = new Departement();
-
-        Mockito.when(etudiantRepository.save(Mockito.any(Etudiant.class))).thenReturn(etudiant);
-        Mockito.when(contratRepository.save(Mockito.any(Contrat.class))).thenReturn(contrat);
-        Mockito.when(equipeRepository.save(Mockito.any(Equipe.class))).thenReturn(equipe);
-        Mockito.when(departementRepository.save(Mockito.any(Departement.class))).thenReturn(departement);
-
-        Etudiant savedEtudiant = etudiantService.updateEtudiant(etudiant);
-
-        assertNotNull(savedEtudiant);
-        assertEquals("John", savedEtudiant.getNomE());
-        assertEquals("Doe", savedEtudiant.getPrenomE());
-        assertEquals(Option.SIM, savedEtudiant.getOp());
-    }
 
 
 
     @Test
-    public void testRemoveEtudiant() {
+    public void testRetrieveAllEtudiant() {
 
-        Etudiant etudiant = new Etudiant();
-        etudiant.setNomE("John");
-        etudiant.setPrenomE("Doe");
-        etudiant.setOp(Option.SIM);
+        List<Etudiant> etudiantList = new ArrayList<>();
+        etudiantList.add(new Etudiant("rym","baccouri",Option.GAMIX));
 
 
-        Mockito.when(etudiantRepository.save(Mockito.any(Etudiant.class)))
-                .thenAnswer(invocation -> {
-                    Etudiant etudiantToSave = invocation.getArgument(0);
-                    etudiantToSave.setIdEtudiant(1); // Assign a valid ID here.
-                    return etudiantToSave;
-                });
-        Etudiant savedEtudiant = etudiantService.addEtudiant(etudiant);
+        when(etudiantRepository.findAll()).thenReturn(etudiantList);
 
+        List<Etudiant> result = etudiantService.retrieveAllEtudiants();
 
-
-        Mockito.when(etudiantRepository.findById(savedEtudiant.getIdEtudiant()))
-                .thenReturn(Optional.of(savedEtudiant));
-
-        Optional<Etudiant> optionalEtudiant = etudiantRepository.findById(savedEtudiant.getIdEtudiant());
-        assertTrue(optionalEtudiant.isPresent());
-
-        etudiantService.removeEtudiant(savedEtudiant.getIdEtudiant());
-
-        Mockito.verify(etudiantRepository).delete(savedEtudiant);
-
-      Optional<Etudiant> removedEtudiant = etudiantRepository.findById(savedEtudiant.getIdEtudiant());
-       assertFalse(removedEtudiant.isPresent());
-
-
+        // Verify that the service method returns the expected result
+        assertEquals(etudiantList, result);
 
     }
 
+    @Test
+    public void testRetrieveEtudiant() {
+
+        Etudiant etudiants= new Etudiant( "rym", "baccouri", Option.GAMIX);
+
+        // Mock the behavior of the courseRepository to return the sampleCourse when findById is called with numCourse
+        when(etudiantRepository.findById(etudiants.getIdEtudiant())).thenReturn(Optional.of(etudiants));
+
+
+        Etudiant retrieveEtudiant = etudiantService.retrieveEtudiant(etudiants.getIdEtudiant());
+
+
+
+        assertEquals(etudiants,retrieveEtudiant);
+
+    }
+    @Test
+    public void testremoveEtudiant() {
+
+        Integer idEtudian = Math.toIntExact(1L);
+
+        // Call the removeCourse function with the numCourse
+        etudiantService.removeEtudiant(idEtudian);
+
+        // Verify that the deleteById method of courseRepository is called with the numCourseToRemove
+        verify(etudiantRepository, times(1)).deleteById(idEtudian);
+    }
 
 
 
